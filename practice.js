f3e22c4f7297ac4a9c039b288a176d9f67a5d8f8
Kevin <kevinkniu@gmail.com>
2022-03-02 17:08:32 -0800

    Passed all practice tests

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -22,7 +22,13 @@ var moreFruits = function(fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
-
+  var output = 0;
+  _.each(numbers, function(number) {
+    if (number % 5 === 0) {
+      output++;
+    }
+  });
+  return output;
 };
 
 /*
@@ -33,18 +39,24 @@ var multiplesOfFive = function(numbers) {
 
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
-
+  return _.filter(fruits, function(fruit) {
+    return fruit === targetFruit;
+  });
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  return _.filter(fruits, function(fruit) {
+    return fruit.charAt(0) === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function(desserts) {
-
+  return _.filter(desserts, function(item) {
+    return item['type'] === 'cookie';
+  });
 };
 
 /*
@@ -55,27 +67,51 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+  return _.reduce(products, function(sum, item) {
+    return sum + Number(item.price.slice(1));
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
-
+  var catArray = [];
+  _.each(desserts, function(item) {
+    catArray.push(item.type);
+  });
+  console.log(catArray);
+  return _.reduce(catArray, function(object, type) {
+    if (object[type] === undefined) {
+      object[type] = 1;
+    } else {
+      object[type]++;
+    }
+    return object;
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+  return _.reduce(movies, function(array, movie) {
+    if (movie['releaseYear'] >= 1990 && movie['releaseYear'] <= 2000) {
+      array.push(movie['title']);
+    }
+    return array;
+  }, []);
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function(movies, timeLimit) {
-
+  return _.reduce(movies, function(output, movie) {
+    if (movie.runtime < timeLimit) {
+      output = true;
+    }
+    return output;
+  }, false);
 };
 
 /*
@@ -87,14 +123,24 @@ var movieNight = function(movies, timeLimit) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function(fruits) {
-
+  return _.map(fruits, function(fruit) {
+    return fruit.toUpperCase();
+  });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function(desserts) {
-
+  return _.map(desserts, function(dessert) {
+    dessert.glutenFree = false;
+    if (dessert.ingredients.some(function(ingredient) {
+      return ingredient === 'flour';
+    })) {
+      dessert.glutenFree = true;
+    }
+    return dessert;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -102,7 +148,7 @@ var glutenFree = function(desserts) {
 //
 // having trouble with decimals? check out this article:
 // http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-// 
+//
 /*
 
  example output:
@@ -111,12 +157,17 @@ var glutenFree = function(desserts) {
     {
       id: 1,
       product: 'Olive Oil',
-      price: '$12.1', 
+      price: '$12.1',
       salePrice: '$9.68'
     }
   ];
 
 */
 var applyCoupon = function(groceries, coupon) {
-
+  return _.map(groceries, function(item) {
+    var price = Number(item.price.slice(1));
+    //originally was using this notation but eslint doesn't like `;
+    item.salePrice = '$' + (Math.round(price * (1 - coupon) * 100) / 100).toFixed(2);
+    return item;
+  });
 };
